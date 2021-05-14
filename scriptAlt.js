e0e49af9b555adc4fc28ad038e8b7e722cba280b
DvDRo5 <dromang5@gmail.com>
2021-05-14 13:09:30 +0200

    add_btn_price

--- scriptAlt.js
+++ scriptAlt.js
@@ -1,4 +1,4 @@
-
+/* 
 let arraySelect=//crea el select con las opciones
 fetch(`https://fakestoreapi.com/products/categories`)
     .then(res=>res.json())
@@ -34,7 +34,18 @@ fetch(`https://fakestoreapi.com/products/categories`)
                         })
                 })          
     }
-
+ */
     document.getElementById("seleccion").addEventListener("change",(event)=>{
         getCategory(event.target.value)
         })
+
+     document.getElementById("togglePrice").addEventListener("click",(e)=>{
+      let btnPrice =  document.getElementById("togglePrice").innerHTML 
+        if(btnPrice == 'Precio ascendente'){
+            document.getElementById("togglePrice").innerHTML = 'Precio descendente'  
+           }else{
+               document.getElementById("togglePrice").innerHTML = 'Precio ascendente' 
+           }
+        })
+        
+        
